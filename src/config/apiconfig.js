ca0359b023b1f7e40043a71fad8ad544f1cd0d0b
syed <syedminhaaj@gmail.com>
2024-10-15 18:30:42 -0400

    added BE render api in config

--- src/config/apiconfig.js
+++ src/config/apiconfig.js
@@ -1,5 +1,5 @@
 const BASE = {
-  API_DEPLOYED_BASE_URL: "http://localhost:3002",
+  API_DEPLOYED_BASE_URL: "https://reactnodesocialmedia-be.onrender.com",
   API_LOCAL_URL: "http://localhost:3002",
   ACCESSTOKEN_HEADER: { header: localStorage.getItem("accessToken") },
 };
